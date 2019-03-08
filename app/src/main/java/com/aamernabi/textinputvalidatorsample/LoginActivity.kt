0ca3e54507ab187f86a6712252e6e0d8febcae02
Aamer Nabi <aamerpaul@gmail.com>
2019-03-08 22:18:52 +0530

    validations

--- app/src/main/java/com/aamernabi/textinputvalidatorsample/LoginActivity.kt
+++ app/src/main/java/com/aamernabi/textinputvalidatorsample/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.aamernabi.inputvalidator.ofLength
+import com.aamernabi.inputvalidator.onTextChanged
 import com.aamernabi.inputvalidator.validate
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.content_login.*
@@ -15,19 +16,29 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         setSupportActionBar(toolbar)
 
-        btn_submit.setOnClickListener {
-            val isValidUsername = et_username.validate(ofLength(4))
-            val isValidPassword = et_password.validate(ofLength(8)) && et_password.validate {
-                PasswordValidator.validate(this)
-            }
+        et_username.onTextChanged {
+            til_username.error = null
+            til_username.isErrorEnabled = false
+        }
 
-            if (!isValidUsername) {
+        et_password.onTextChanged {
+            til_password.error = null
+            til_password.isErrorEnabled = false
+        }
+
+        btn_submit.setOnClickListener {
+            et_username.validate(ofLength(4), {
                 til_username.error = "Invalid username. Minimum 4 characters required"
                 return@setOnClickListener
-            } else if (!isValidPassword) {
+            })
+            et_password.validate(ofLength(8), {
+                til_password.error = "Invalid password. Minimum 8 characters required"
+                return@setOnClickListener
+            })
+            et_password.validate({ PasswordValidator.validate(this) }, {
                 til_password.error = "Password must contain mixture of letter, numbers and special characters"
                 return@setOnClickListener
-            }
+            })
 
             Toast.makeText(this@LoginActivity, "Valid details", Toast.LENGTH_LONG).show()
         }
--- inputvalidator/src/main/java/com/aamernabi/inputvalidator/InputValidator.kt
+++ inputvalidator/src/main/java/com/aamernabi/inputvalidator/InputValidator.kt
@@ -1,9 +1,12 @@
 package com.aamernabi.inputvalidator
 
+import android.support.design.widget.TextInputLayout
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 
 val notEmpty: String.() -> Boolean = { isNotEmpty() }
-fun ofLength(length: Int): String.() -> Boolean = { this.length == length  }
+fun ofLength(length: Int): String.() -> Boolean = { this.length >= length  }
 fun ofLength(min: Int, max: Int): String.() -> Boolean = { this.length in min..max }
 
 inline fun EditText.validate(block: String.() -> Boolean): Boolean {
@@ -11,8 +14,35 @@ inline fun EditText.validate(block: String.() -> Boolean): Boolean {
     return block(text)
 }
 
-fun test(e: EditText) {
+inline fun EditText.validate(validator: String.() -> Boolean, block: () -> Unit): Boolean {
+    val text = text?.toString() ?: ""
+    val isValid = validator(text)
+
+    if (!isValid) {
+        block()
+    }
+
+    return isValid
+}
+
+fun EditText.onTextChanged(block: () -> Unit) {
+    addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            block()
+        }
+    })
+}
+
+fun test(e: EditText, til: TextInputLayout) {
     val isNotEmpty = e.validate(notEmpty)
     val hasValidLen = e.validate(ofLength(6))
     val hasDigits = e.validate { contains("\\d".toRegex()) }
+
+    val hasCaps = e.validate({ contains("A-Z".toRegex())}, {
+        til.error = "Must contain Caps characters"
+    })
 }
